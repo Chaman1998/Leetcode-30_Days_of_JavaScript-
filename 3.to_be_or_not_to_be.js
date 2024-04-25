74391802807ff5ea778356ae9db2fa4e73f2b3d5
Chaman Sarkar <84088334+Chaman1998@users.noreply.github.com>
2024-04-25 13:26:20 +0000

    done

---
+++ 3.to_be_or_not_to_be.js
@@ -0,0 +1,24 @@
+var expect = function (val) {
+    return {
+        toBe: function (expectedVal) {
+            if (val === expectedVal) {
+                return true;
+            }
+            else {
+                throw new Error("Not Equal");
+            }
+        },
+        notToBe: function (unexpectedVal) {
+            if (val !== unexpectedVal) {
+                return true;
+            } else {
+                throw new Error("Equal");
+            }
+        }
+    };
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
